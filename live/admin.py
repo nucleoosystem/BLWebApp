b3eca4da02c655fe364b67ca107af7b13e64514c
Lawrence Mainja <lmainja@udel.edu>
2018-06-07 00:34:37 -0500

    Initial commit

---
+++ live/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Teachings
+
+admin.site.register(Teachings)
+
+# Register your models here.
---
+++ live/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Welcome to the Live Stream Site Powered by The Black Lemur Project")
+
+# Create your views here.
